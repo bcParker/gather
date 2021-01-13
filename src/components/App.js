d51a057bf2beaf0c6c57c3a181a3670244cfb076
Chase Parker <cparker8890@gmail.com>
2021-01-12 23:29:02 -0500

    changes to file structure as well as added background color change

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import '../css/App.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+        GATHER
+      </header>
+      <div>
+        <p>
+          Hello World!
+        </p>
+      </div>
+    </main>
+  );
+}
+
+export default App;
